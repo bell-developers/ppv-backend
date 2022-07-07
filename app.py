88e8390f50f7355706e373c9a6424cd135eb8505
Luciano Aieta <lgaieta.dev@gmail.com>
2022-07-07 18:23:07 -0300

    Added product endpoint example

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def getProducts():
     return jsonify(falseProducts)
 
 
+@app.route('/product/<id>')
+def getSingularProduct(id):
+    return "El id de su producto es: " + id
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
