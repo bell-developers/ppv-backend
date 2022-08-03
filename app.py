e93703fcc4e5e030eaa0c278ef4b1b4177fefc7e
Luciano Aieta <lgaieta.dev@gmail.com>
2022-08-03 14:21:07 -0300

    Added getProducts by number

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
+from urllib import response
 from flask import Flask, jsonify
 from falseData import falseProducts
+from flask_cors import CORS
+
+from genProducts import genProducts
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/catalog')
+@app.route('/catalog/')
 def getProducts():
     return jsonify(falseProducts)
+
+
+@app.route('/catalog/<times>')
+def getProductsMock(times):
+    numberOfProducts = int(times)
+    return jsonify(genProducts(numberOfProducts))
 
 
 @app.route('/product/<id>')
--- genProducts.py
+++ genProducts.py
@@ -1,12 +1,18 @@
 from openImage import openImage
 
 
-falseProducts = [
-    {
-        "id": 1,
+def genFalseData(id: int):
+    return {
+        "id": id,
         "name": "Remera Naranja",
         "price": 5000,
         "sizes": ['S', 'M'],
         "images": [openImage(r'C:\Programming\Projects\Bell\ppv\ppv-backend\images\remera-naranja.png')]
-    },
-]
+    }
+
+
+def genProducts(times: int):
+    products = []
+    for i in range(times):
+        products.append(genFalseData(i))
+    return products
